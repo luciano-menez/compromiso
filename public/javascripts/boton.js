11c6cc5e4cc07eb9c0d654871e9fc21ec7d9fbed
Tabitoo <lautarovaldez4@gmail.com>
2021-11-18 10:03:04 -0300

    Se agrego nuevo boton flotante

--- public/javascripts/boton.js
+++ public/javascripts/boton.js
@@ -1,20 +1,12 @@
 const bntMenu = document.querySelector('#menu')
 const menuContent = document.querySelector('.menu-content')
 
-let position = 0
-
-
-position = window.screenY
-console.log(position)
-
-
 
 addEventListener("DOMContentLoaded", () => {
 
     const footer = document.querySelector('.footer');
 
     let botonStatic = (entry) => {
-        console.log(entry)
 
         entry.forEach(element => {
 
--- public/javascripts/boton2.js
+++ public/javascripts/boton2.js
@@ -1,9 +1,32 @@
 const boton = document.querySelector("#Boton");
 const botones = document.querySelector(".menu-contact");
+const contenedor = document.querySelector(".contactanos-contenedor")
 const contactImage = document.querySelector('.contact-image');
 const menu = document.querySelector("#Menu2");
 
-console.log(menu)
+const footer = document.querySelector('.footer');
+
+let botonStatic = (entry) => {
+  
+
+    entry.forEach(element => {
+
+        if (element.isIntersecting) {
+            contenedor.classList.add('menu-static')
+        } else {
+            contenedor.classList.remove('menu-static')
+        }
+
+    });
+
+}
+
+const observer = new IntersectionObserver(botonStatic, {
+
+    threshold: 0.25
+});
+
+observer.observe(footer);
 
 boton.addEventListener("click", () => {
     botones.classList.toggle('boton-block')
